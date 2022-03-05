acfc775e167e5fb7bc596d232c1e47c4e3fd3783
ralph.moran <ralphmoran2003@gmail.com>
2022-03-04 17:47:36 -0800

    Added autoloader

--- index.php
+++ index.php
@@ -1,21 +1,19 @@
 <?php
+
+$dirname = dirname(__FILE__);
+
+# Autoloader
+require_once $dirname . '/lib/autoload.php';
+
 # Helper functions
-include_once 'lib/helper.functions.php';
+require_once $dirname . '/lib/helper.functions.php';
 
-# include UITestCase class and Tester class
-include_once 'lib/UIFormatter.php';
-include_once 'lib/UITestCase.php';
-include_once 'lib/UITester.php';
+# Examples
+require_once $dirname . '/examples/functions.php';
+require_once $dirname . '/examples/classes/classes.php';
 
-# Include functions
-include_once 'testcases/functions.php';
-
-# Include classes
-include_once 'testcases/classes/classes.php';
-
-
+# Use case
 $tester = new UITester();
 
 $tester->only('GetRandomStrTest')
 	->outputAssertionResults();
-
--- lib/helper.functions.php
+++ lib/helper.functions.php
@@ -2,6 +2,12 @@
 
 if (! function_exists('env') )
 {
+	/**
+	 * Returns the value of the environment variable.
+	 *
+	 * @param string $env_var
+	 * @return mixed
+	 */
 	function env( $env_var ) 
 	{
 		if( isset( $_ENV[$env_var] ) || array_key_exists( $env_var, $_ENV ) )
@@ -11,6 +17,11 @@ if (! function_exists('env') )
 
 if( ! function_exists('load_env') )
 {
+	/**
+	 * Loads the entire environment for the application.
+	 *
+	 * @return void
+	 */
 	function load_env()
 	{
 		if( file_exists('./.env') )
