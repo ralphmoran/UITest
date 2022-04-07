9d0b989932e579e9b588decd8878b1a671c590a1
ralph.moran <ralphmoran2003@gmail.com>
2022-04-07 14:48:07 -0700

    Added new ObjectAssertions

--- lib/traits/ObjectAssertions.php
+++ lib/traits/ObjectAssertions.php
@@ -5,87 +5,35 @@ use App\UITesting\Lib\Classes\UITestCase;
 trait ObjectAssertions
 {
 	/**
-	 * Asserts if a key exists in the array.
+	 * Asserts if two objects are identical if and only if both of them reference 
+	 * the same instance of a class.
 	 * 
-	 * This assertion will return true even though the value of the key is NULL.
-	 *
-	 * @param string $key
-	 * @param array $array
+	 * @param object $obj1
+	 * @param object $obj2
 	 * @return UITestCase
 	 */
-	protected function assertObjectEquals() : UITestCase
+	protected function assertObjectEqualsByReference( $obj1, $obj2 ) : UITestCase
 	{
 		return $this->logAssertionStatus(__FUNCTION__, 
 											get_defined_vars(), 
-											is_dir($dir)
+											( $obj1 === $obj2 )
 										);
 	}
 	
 	/**
-	 * Asserts if a key exists in the array.
-	 * 
-	 * This assertion will return true even though the value of the key is NULL.
+	 * Asserts if two objects are instances of the same class and have the same 
+	 * properties and values.
 	 *
-	 * @param string $key
-	 * @param array $array
+	 * @param object $obj1
+	 * @param object $obj2
 	 * @return UITestCase
 	 */
-	protected function assertObjectHasAttribute() : UITestCase
+	protected function assertObjectEqualsByValues( $obj1, $obj2 ) : UITestCase
 	{
 		return $this->logAssertionStatus(__FUNCTION__, 
 											get_defined_vars(), 
-											is_dir($dir)
+											( $obj1 == $obj2 )
 										);
 	}
 
-	/**
-	 * Asserts if a key exists in the array.
-	 * 
-	 * This assertion will return true even though the value of the key is NULL.
-	 *
-	 * @param string $key
-	 * @param array $array
-	 * @return UITestCase
-	 */
-	protected function assertObjectHasStaticAttribute() : UITestCase
-	{
-		return $this->logAssertionStatus(__FUNCTION__, 
-											get_defined_vars(), 
-											is_dir($dir)
-										);
-	}
-
-	/**
-	 * Asserts if a key exists in the array.
-	 * 
-	 * This assertion will return true even though the value of the key is NULL.
-	 *
-	 * @param string $key
-	 * @param array $array
-	 * @return UITestCase
-	 */
-	protected function assertObjectHasMethod() : UITestCase
-	{
-		return $this->logAssertionStatus(__FUNCTION__, 
-											get_defined_vars(), 
-											is_dir($dir)
-										);
-	}
-
-	/**
-	 * Asserts if a key exists in the array.
-	 * 
-	 * This assertion will return true even though the value of the key is NULL.
-	 *
-	 * @param string $key
-	 * @param array $array
-	 * @return UITestCase
-	 */
-	protected function assertObjectHasStaticMethod() : UITestCase
-	{
-		return $this->logAssertionStatus(__FUNCTION__, 
-											get_defined_vars(), 
-											is_dir($dir)
-										);
-	}
 }
